24e27daf1c5819c034e266b239331f19112e13dd
Souptik_Banerjee <Souptik_Banerjee@Dell.com>
2018-08-07 23:06:19 +0530

    Commit 1

---
+++ HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CloudValidator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+            Console.WriteLine("Hello");
+        }
+
+        public ActionResult Admin()
+        {
+            return View();
+            var config = new ConfigurationBuilder();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ HomeController1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CloudValidator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+            File.Write("Hello WOrld");
+        }
+
+        public ActionResult Admin()
+        {
+            return View();
+            ConfigurationManager.AppSettings["heloo"];
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
